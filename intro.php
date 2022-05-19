349ee88351be671e2a5685137d12c1a11f20ac0f
JardinEgwa <egwajardin@gmail.com>
2022-05-19 12:50:00 +0300

    first php

---
+++ intro.php
@@ -0,0 +1,24 @@
+
+<?php
+    echo "This is my first php file";
+    echo "<br>";
+    echo 15 + 30;
+    echo "<br>";
+    echo "My name is JARDIN EGWA";
+    echo "<br>";
+    echo "The sum of 15 and 20 is;". 15+20;
+    ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,16 @@
+<?php
+$name="Jardin Egwa";
+$schoolname="eMobilis Institute";
+$num1="40";
+$num2="20";
+
+echo"My name is $name";
+echo "<br>";
+echo"My school is $schoolname";
+echo "<br>";
+echo"The sum of $num1 and $num2 is " . 40+20;
+echo "<br>";
+echo "The product of $num2 and $num1 is " . 40*20;
+echo "<br>";
+echo "The difference between $num1 and $num2 is " . 40-20;
+echo "<br";
