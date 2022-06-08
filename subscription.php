51aac42a092946953a47c28cd86a7ed23c1a94d8
JardinEgwa <egwajardin@gmail.com>
2022-06-08 11:50:09 +0300

    new

---
+++ subscription.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FORM VALIDATION</title>
+</head>
+<body>
+<h2>SUBSCRIPTION FORM</h2>
+<form action="">
+    <label for="Name">Name:</label>
+    <input type="text"id="Name">
+    <br>
+    <label for="email">E-mail:</label>
+    <input type="email"id="email">
+    <br>
+    <label for="Website">Website:</label>
+    <input type="text"id="Website">
+    <br>
+    <label for="Comment">Comment:</label>
+    <textarea name="comment" id="Comment" cols="40" rows="5"></textarea>
+    <br>
+    <label for="Gender">Gender:</label>
+    <input type="radio"name="Gender" value="Female">Female
+    <input type="radio"name="Gender" value="Male">Male
+    <input type="radio"name="Gender" value="Other">Other
+    <br>
+    <input type="submit"value="Submit">
+
+
+</form>
+</body>
+</html>
